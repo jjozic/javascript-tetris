e6b4da7bdc7dc6b6197719dab6853597ab1f06eb
jjozic <jjozic98@gmail.com>
2020-05-23 15:35:03 +0200

    started work on logic
    - added 3 files
    - started working on JS logic for the tetrominoes
    - first grid styling already done in CSS file

---
+++ app.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid')
+    let squares = document.querySelectorAll('.grid div')
+    const scoreDisplay = document.querySelector('#score')
+    const width = 10
+}) 
