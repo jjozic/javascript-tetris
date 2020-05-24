441158a3f1837c289b05f6a3277d0faa9540c3ae
jjozic <jjozic98@gmail.com>
2020-05-24 23:18:08 +0200

    Started working on the logic

--- app.js
+++ app.js
@@ -1,6 +1,70 @@
 document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid')
-    let squares = document.querySelectorAll('.grid div')
+    let squares = Array.from(document.querySelectorAll('.grid div'))
     const scoreDisplay = document.querySelector('#score')
+    const startBtn = document.querySelector('#start-button')
     const width = 10
+
+    //the tetrominoes
+    const lTetromino = [
+        [1, width+1, width*2+1, 2],
+        [width, width+1, width+2, width*2+2],
+        [1, width+1, width*2+1, width*2],
+        [width, width*2, width*2+1, width*2+2]
+    ]
+
+    const zTetromino = [
+        [0,width,width+1,width*2+1],
+        [width+1, width+2,width*2,width*2+1],
+        [0,width,width+1,width*2+1],
+        [width+1, width+2,width*2,width*2+1]
+      ]
+    
+      const tTetromino = [
+        [1,width,width+1,width+2],
+        [1,width+1,width+2,width*2+1],
+        [width,width+1,width+2,width*2+1],
+        [1,width,width+1,width*2+1]
+      ]
+    
+      const oTetromino = [
+        [0,1,width,width+1],
+        [0,1,width,width+1],
+        [0,1,width,width+1],
+        [0,1,width,width+1]
+      ]
+    
+      const iTetromino = [
+        [1,width+1,width*2+1,width*3+1],
+        [width,width+1,width+2,width+3],
+        [1,width+1,width*2+1,width*3+1],
+        [width,width+1,width+2,width+3]
+      ]
+
+      const tetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
+
+      let currentPostion = 4
+      let currentRotation = 0
+
+      //random select tetromino
+      let random = Math.floor(Math.random()*tetrominoes.length)
+      let current = tetrominoes[random][0]
+
+      //draw the tetromino
+      function draw(){
+          current.forEach(index => {
+              squares[currentPostion + index].classList.add('tetromino')
+          })
+      }
+
+      //undraw the tetromino
+      function undraw(){
+        current.forEach(index => {
+            squares[currentPostion + index].classList.remove('tetromino')
+        })
+      }
+
+      draw()
+
+
 }) 
